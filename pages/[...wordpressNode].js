d7c0ae8f448463bd718438809c1930c1685fdf82
allanconcepcion <73856850+allanconcepcion@users.noreply.github.com>
2025-03-31 12:51:52 -0700

    Update [...wordpressNode].js
    restore the update

--- pages/[...wordpressNode].js
+++ pages/[...wordpressNode].js
@@ -4,17 +4,8 @@ export default function Page(props) {
   return <WordPressTemplate {...props} />;
 }
 
-// export function getStaticProps(ctx) {
-//   return getWordPressProps({ ctx });
-// }
-export async function getStaticProps(ctx) {
-  try {
-    const props = await getWordPressProps({ ctx });
-    return props;
-  } catch (error) {
-    console.error("Error fetching WordPress data:", error);
-    return { notFound: true }; // Avoids breaking the build
-  }
+export function getStaticProps(ctx) {
+  return getWordPressProps({ ctx });
 }
 
 export async function getStaticPaths() {
